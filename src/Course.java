40d67e3b3450ab228e383e46fa57581a78ca05bc
YuGuo-AlreadyTaken <guoyu.322.95@gmail.com>
2017-02-13 22:37:47 -0500

    Added Course class, and modified Student and Instructor classes

---
+++ src/Course.java
@@ -0,0 +1,13 @@
+/**
+ * Created by yguo289 on 13/02/17.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String descroption){
+        this.cname = cname;
+        this.description = descroption;
+    }
+
+}
